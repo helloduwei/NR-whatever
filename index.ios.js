df409ca9595bfdf5603d66e22bc51069e163afce
duwei <helloduwei@hotmail.com>
2015-10-08 17:53:40 +0800

    iosjs init

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
+
 'use strict';
 
 var React = require('react-native');
@@ -17,14 +14,7 @@ var whatever = React.createClass({
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          Welcome!
         </Text>
       </View>
     );
@@ -36,18 +26,16 @@ var styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#f2f2f2',
   },
   welcome: {
     fontSize: 20,
+    color: '#b13223',
+    fontWeight: 'bold',
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  
 });
 
 AppRegistry.registerComponent('whatever', () => whatever);
